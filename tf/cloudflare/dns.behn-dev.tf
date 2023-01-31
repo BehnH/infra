fc6ab697d643fe901d0aef8121538cbadd5d40c2
Behn <7383025+BehnH@users.noreply.github.com>
2023-01-31 09:59:08 +0000

    tf/dns: Update cloudflare records

--- tf/cloudflare/dns.behn-dev.tf
+++ tf/cloudflare/dns.behn-dev.tf
@@ -1,3 +1,12 @@
+resource "cloudflare_record" "A_behn-002E-dev_identity" {
+  name    = "id.behn.dev"
+  proxied = false
+  ttl     = "1"
+  type    = "A"
+  value   = "212.71.236.166"
+  zone_id = cloudflare_zone.tfer--behn-002E-dev
+}
+
 resource "cloudflare_record" "tfer--CNAME_behn-002E-dev_7680c18fad65c0fae19eac858af048a6" {
   name    = "_domainconnect.behn.dev"
   proxied = "true"
@@ -25,6 +34,46 @@ resource "cloudflare_record" "CNAME_behn-002E-dev_www" {
   zone_id = cloudflare_zone.tfer--behn-002E-dev.id
 }
 
+resource "cloudflare_record" "MX_behn-002E-dev_google-workspace-mx-01" {
+  name     = "behn.cc"
+  priority = "5"
+  proxied  = "false"
+  ttl      = "1"
+  type     = "MX"
+  value    = "alt1.aspmx.l.google.com"
+  zone_id  = cloudflare_zone.tfer--behn-002E-dev.id
+}
+
+resource "cloudflare_record" "MX_behn-002E-dev_google-workspace-mx-02" {
+  name     = "behn.cc"
+  priority = "5"
+  proxied  = "false"
+  ttl      = "1"
+  type     = "MX"
+  value    = "alt2.aspmx.l.google.com"
+  zone_id  = cloudflare_zone.tfer--behn-002E-dev.id
+}
+
+resource "cloudflare_record" "MX_behn-002E-dev_google-workspace-mx-03" {
+  name     = "behn.cc"
+  priority = "10"
+  proxied  = "false"
+  ttl      = "1"
+  type     = "MX"
+  value    = "alt3.aspmx.l.google.com"
+  zone_id  = cloudflare_zone.tfer--behn-002E-dev.id
+}
+
+resource "cloudflare_record" "MX_behn-002E-dev_google-workspace-mx-04" {
+  name     = "behn.cc"
+  priority = "10"
+  proxied  = "false"
+  ttl      = "1"
+  type     = "MX"
+  value    = "alt4.aspmx.l.google.com"
+  zone_id  = cloudflare_zone.tfer--behn-002E-dev.id
+}
+
 resource "cloudflare_record" "TXT_behn-0023-dev_google-site-verification" {
   name    = "behn.dev"
   proxied = "false"
@@ -35,10 +84,10 @@ resource "cloudflare_record" "TXT_behn-0023-dev_google-site-verification" {
 }
 
 resource "cloudflare_record" "TXT_behn-0023-dev_keybase-site-verification" {
-  name = "behn.dev"
+  name    = "behn.dev"
   proxied = "false"
-  ttl = "1"
-  type = "TXT"
-  value = "keybase-site-verification=iLIJ7fiVMBmiPOZ0JTb07W6DS8RC26OcCBsJtJrJk5E"
+  ttl     = "1"
+  type    = "TXT"
+  value   = "keybase-site-verification=iLIJ7fiVMBmiPOZ0JTb07W6DS8RC26OcCBsJtJrJk5E"
   zone_id = cloudflare_zone.tfer--behn-002E-dev.id
 }
