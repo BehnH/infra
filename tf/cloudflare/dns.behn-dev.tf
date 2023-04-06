59cbbf03d30263b109f2a4ef9a1589f1dbe4566d
Behn <7383025+BehnH@users.noreply.github.com>
2023-04-06 16:46:00 +0100

    dns: Add n8n route

--- tf/cloudflare/dns.behn-dev.tf
+++ tf/cloudflare/dns.behn-dev.tf
@@ -52,6 +52,15 @@ resource "cloudflare_record" "A_behn-dev_kiali" {
   zone_id = cloudflare_zone.tfer--behn-002E-dev.id
 }
 
+resource "cloudflare_record" "A_behn-dev_n8n" {
+  name    = "flow.svc.behn.dev"
+  proxied = false
+  ttl     = "1"
+  type    = "A"
+  value   = "212.71.236.56"
+  zone_id = cloudflare_zone.tfer--behn-002E-dev.id
+}
+
 resource "cloudflare_record" "tfer--CNAME_behn-002E-dev_7680c18fad65c0fae19eac858af048a6" {
   name    = "_domainconnect.behn.dev"
   proxied = "true"
