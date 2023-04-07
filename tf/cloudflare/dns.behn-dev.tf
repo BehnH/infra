a1e6c536e0f095b43bef007b4ee9a63c92e36cad
Behn <7383025+BehnH@users.noreply.github.com>
2023-04-07 11:20:47 +0100

    dns: Add spotify dashboard

--- tf/cloudflare/dns.behn-dev.tf
+++ tf/cloudflare/dns.behn-dev.tf
@@ -61,6 +61,15 @@ resource "cloudflare_record" "A_behn-dev_n8n" {
   zone_id = cloudflare_zone.tfer--behn-002E-dev.id
 }
 
+resource "cloudflare_record" "A_behn-dev_spotify-dash" {
+  name    = "spotify.svc.behn.dev"
+  proxied = false
+  ttl     = "1"
+  type    = "A"
+  value   = "212.71.236.56"
+  zone_id = cloudflare_zone.tfer--behn-002E-dev.id
+}
+
 resource "cloudflare_record" "tfer--CNAME_behn-002E-dev_7680c18fad65c0fae19eac858af048a6" {
   name    = "_domainconnect.behn.dev"
   proxied = "true"
