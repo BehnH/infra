58bf2947f56d77606b3242aff3e5ea39f0fa2c15
Behn <7383025+BehnH@users.noreply.github.com>
2023-07-05 12:47:37 +0100

    dns: Add new vault record

--- tf/cloudflare/dns.behn-dev.tf
+++ tf/cloudflare/dns.behn-dev.tf
@@ -88,6 +88,15 @@ resource "cloudflare_record" "A_behn-dev_crt" {
   zone_id = cloudflare_zone.tfer--behn-002E-dev.id
 }
 
+resource "cloudflare_record" "A_svc-behn-dev_vault" {
+  name    = "vault.svc.behn.dev"
+  proxied = false
+  ttl     = "1"
+  type    = "A"
+  value   = "142.132.240.34"
+  zone_id = cloudflare_zone.tfer--behn-002E-dev.id
+}
+
 resource "cloudflare_record" "tfer--CNAME_behn-002E-dev_7680c18fad65c0fae19eac858af048a6" {
   name    = "_domainconnect.behn.dev"
   proxied = "true"
