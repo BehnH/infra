3c909e9a7939a6882868a6c6a92f9fb1217b087e
Behn <7383025+BehnH@users.noreply.github.com>
2023-07-11 22:49:47 +0100

    tf/dns: Add headscale

--- tf/cloudflare/dns.behn-dev.tf
+++ tf/cloudflare/dns.behn-dev.tf
@@ -142,6 +142,15 @@ resource "cloudflare_record" "A_behn-dev_s3-console" {
   zone_id = cloudflare_zone.tfer--behn-002E-dev.id
 }
 
+resource "cloudflare_record" "A_behn-dev_vpn" {
+  name    = "vpn.behn.dev"
+  proxied = false
+  ttl     = "1"
+  type    = "A"
+  value   = "142.132.244.5"
+  zone_id = cloudflare_zone.tfer--behn-002E-dev.id
+}
+
 
 resource "cloudflare_record" "tfer--CNAME_behn-002E-dev_7680c18fad65c0fae19eac858af048a6" {
   name    = "_domainconnect.behn.dev"
